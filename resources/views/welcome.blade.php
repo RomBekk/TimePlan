fb1ae556f79c1c95b81e542e326468f1b0f42455
BVAk <bekkviktoria55@gmail.com>
2018-10-20 17:35:34 +0300

    add page TimePlan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="ico_black.ico" type="image/x-icon"></link>
+        <link rel="shortcut icon" href="ico_black.ico" type="image/x-icon">
         <title>TimePlan</title>
 
         <!-- Fonts -->
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: black;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
@@ -75,23 +75,25 @@
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
                     @endauth
-                </div>
+                
+                </div> 
             @endif
+            <div>
+                    <img src="logo_black2.png" width="50%" style="background:white">
+                </div>
 
             <div class="content">
                 <div class="title m-b-md">
-                    <img src="logo_black.png">
+                    
+                </div>
+                
+                <div class="links">
+                    <a href="/TimePlan">Let's start!</a>
                     
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
+
     </body>
 </html>
